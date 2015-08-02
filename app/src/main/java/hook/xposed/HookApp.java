137cef8cf73563a7304ce2c5fc142671d02aee10
nnn7h <83500201@163.com>
2015-08-02 19:24:38 +0800

    udata SharedPreferences

--- app/src/main/java/hook/xposed/HookApp.java
+++ app/src/main/java/hook/xposed/HookApp.java
@@ -21,42 +21,25 @@ public class HookApp implements IXposedHookLoadPackage {
     public void handleLoadPackage(LoadPackageParam loadPackageParam) {
         appList = getHookPkg();
         if (appList == null || !appList.contains(loadPackageParam.packageName)) {
-            XposedBridge.log("wo cao ni ma");
             return;
         }
-
-        XposedBridge.log(loadPackageParam.packageName);
 
         hook(XAbstractHttpClient.getInstance(), loadPackageParam);
         hook(XActivity.getInstance(), loadPackageParam);
         hook(XActivityManager.getInstance(), loadPackageParam);
-        hook(XActivityThread.getInstance(), loadPackageParam);
         hook(XApplicationPackageManager.getInstance(), loadPackageParam);
-        hook(XAssetManager.getInstance(), loadPackageParam);
         hook(XAudioRecord.getInstance(), loadPackageParam);
 
         hook(XBroadcastReceiver.getInstance(), loadPackageParam);
-//        hook(XClass.getInstance(), loadPackageParam);
-        hook(XConnectivityManager.getInstance(), loadPackageParam);
-        hook(XContext.getInstance(), loadPackageParam);
-        hook(XContextImpl.getInstance(), loadPackageParam);
         hook(XContentResolver.getInstance(), loadPackageParam);
-//        hook(XDexClassLoader.getInstance(), loadPackageParam);
-        hook(XDialog.getInstance(), loadPackageParam);
         hook(XMediaRecorder.getInstance(), loadPackageParam);
         hook(XNotificationManager.getInstance(), loadPackageParam);
         hook(XProcessBuilder.getInstance(), loadPackageParam);
         hook(XRuntime.getInstance(), loadPackageParam);
         hook(XSmsManger.getInstance(), loadPackageParam);
-        hook(XSmsMessage.getInstance(), loadPackageParam);
-        hook(XString.getInstance(), loadPackageParam);
         hook(XTelephoneyManager.getInstance(), loadPackageParam);
         hook(XURL.getInstance(), loadPackageParam);
         hook(XWebView.getInstance(), loadPackageParam);
-        hook(XViewGroup.getInstance(), loadPackageParam);
-//        hook(XWindowManageService.getInstance(), loadPackageParam);
-        hook(XWifiManager.getInstance(), loadPackageParam);
-        hook(XBaseDexClassLoader.getInstance(), loadPackageParam);
     }
 
     public void hook(XHook xhook, LoadPackageParam packageParam) {
