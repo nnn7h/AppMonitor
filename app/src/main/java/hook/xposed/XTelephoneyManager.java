fa70c7d1a0404070c7ffd49b737e46a0e8b0481f
nnn7h <83500201@163.com>
2015-08-02 13:50:02 +0800

    udata SharedPreferences

--- app/src/main/java/hook/xposed/XTelephoneyManager.java
+++ app/src/main/java/hook/xposed/XTelephoneyManager.java
@@ -1,5 +1,8 @@
 package hook.xposed;
 
+import util.Logger;
+import util.Stack;
+import util.Util;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
@@ -24,15 +27,28 @@ public class XTelephoneyManager extends XHook {
                     protected void beforeHookedMethod(MethodHookParam param){
                         param.setResult("356357045618430");
                     }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        Logger.log("[=== getDeviceId ===]");
+                        Logger.log("[=== getDeviceId ===] " + Stack.getCallRef());
+                    }
                 });
 
         XposedHelpers.findAndHookMethod(className, packageParam.classLoader,
                 "getLine1Number", new XC_MethodHook() {
 
                     @Override
-                    protected void beforeHookedMethod(MethodHookParam param){
+                    protected void beforeHookedMethod(MethodHookParam param) {
                         param.setResult("13826290651");
                     }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        Logger.log("[=== getLine1Number ===]");
+                        Logger.log("[=== getLine1Number ===] " + Stack.getCallRef());
+                    }
+
                 });
 
         XposedHelpers.findAndHookMethod(className, packageParam.classLoader,
@@ -42,6 +58,13 @@ public class XTelephoneyManager extends XHook {
                     protected void beforeHookedMethod(MethodHookParam param){
                         param.setResult("460006203280876");
                     }
+
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        Logger.log("[=== getSubscriberId ===]");
+                        Logger.log("[=== getSubscriberId ===] " + Stack.getCallRef());
+                    }
+
                 });
 
         XposedHelpers.findAndHookMethod(className, packageParam.classLoader,
