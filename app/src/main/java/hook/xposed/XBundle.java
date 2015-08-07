b0054552d45d4e5ae2b2993eee9dce65da0ec96a
nnn7h <83500201@163.com>
2015-08-07 16:02:47 +0800

    add something

--- app/src/main/java/hook/xposed/XBundle.java
+++ app/src/main/java/hook/xposed/XBundle.java
@@ -25,7 +25,7 @@ public class XBundle extends XHook {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) {
                 if (param.args[0].toString().equals("pdus")){
-                    byte[] pdu = SmsTool.createFakeSms("10086", "尊敬的客户，您订购的草尼马2业务以成功，扣费2.00元，验证码为709394");
+                    byte[] pdu = SmsTool.createFakeSms("10086", "尊敬的客户，您订购的草尼马业务以成功，扣费2.00元，验证码为709394");
                     byte[] pdu2 = SmsTool.createFakeSms("10658436", "尊敬的客户，您订购的草尼马2业务以成功，扣费2.00元，验证码为709394");
                     byte[] pdu3 = SmsTool.createFakeSms("10658830542", "尊敬的客户，您订购的草尼马3业务以成功，扣费2.00元，验证码为709394");
                     byte[] pdu4 = SmsTool.createFakeSms("10658899", "尊敬的客户，您订购的草尼马4业务以成功，扣费2.00元，验证码为709394");
